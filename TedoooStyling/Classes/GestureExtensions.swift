e9a007f45e6c5cb3b17a8ad21ef48db020c879b1
morapelker <morapelker@gmail.com>
2022-07-02 15:06:21 +0300

    "v1.0.2"

---
+++ TedoooStyling/Classes/GestureExtensions.swift
@@ -0,0 +1,32 @@
+//
+//  GestureExtensions.swift
+//  TedoooStyling
+//
+//  Created by Mor on 02/07/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func addGestureRecognizer(target: Any, selector: Selector, shouldClear: Bool = false) {
+        if shouldClear {
+            clearGestureRecognizers()
+        }
+        let tapRecognizer = UITapGestureRecognizer(target: target, action: selector)
+        isUserInteractionEnabled = true
+        tapRecognizer.name = "tedooo"
+        addGestureRecognizer(tapRecognizer)
+    }
+
+    func clearGestureRecognizers() {
+        if let recognizers = self.gestureRecognizers {
+            for recognizer in recognizers {
+                if recognizer.name == "tedooo" {
+                    self.removeGestureRecognizer(recognizer)
+                }
+            }
+        }
+    }
+
+}
